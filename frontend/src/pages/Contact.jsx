4cfb58998460227c9ba97e2ac29071dd48456378
Saucy Corentin <saucy.corentin@gmail.com>
2022-07-21 20:40:43 +0200

    Creation de la page contact et ajout de la méthode POST

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Contact from "../components/Mailer";
 
-export default function Contact() {
+export default function Contacte() {
   return (
     <div>
       <Navbar />
+      <Contact />
       <Footer />
     </div>
   );
