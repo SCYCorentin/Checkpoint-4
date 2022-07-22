3215da10d1b195f2704b652cb90fcfff5201e5c9
Saucy Corentin <saucy.corentin@gmail.com>
2022-07-22 12:30:38 +0200

    last hotfix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import Contact from "./pages/Contact";
 import Faq from "./pages/Faq";
-import Who from "./pages/Who";
 import Admin from "./pages/Admin";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
@@ -13,7 +12,6 @@ function Main() {
     <Router>
       <Routes>
         <Route exact path="/faq" element={<Faq />} />
-        <Route exact path="/who" element={<Who />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/admin" element={<Admin />} />
         <Route exact path="/" element={<App />} />
